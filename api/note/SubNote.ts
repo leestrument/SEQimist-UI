47d01b14a9be6b2e3e9dc28a593d50332e39c600
이혁기 <leestrument.github@gmail.com>
2021-02-03 08:29:38 +0900

    Cleaned SubNote

--- api/note/SubNote.ts
+++ api/note/SubNote.ts
@@ -1,36 +1,15 @@
-export const SUB_NOTE_VELOCITY_MIN = 1
-export const SUB_NOTE_VELOCITY_MAX = 127
-export const SUB_NOTE_VELOCITY_DEF = 64
-export const SUB_NOTE_GATE_DEF     = 1
-
 export class SubNote {
 
     constructor(
 
-        private _velocity   = SUB_NOTE_VELOCITY_DEF,
-        private _gate       = SUB_NOTE_GATE_DEF,
+        private _velocity   = 64,
+        private _gate       = 1,
 
     ){}
 
-    public setVelocity(velocity: number): void {
-
-        this._velocity = velocity
-
-    }
-    public setGate(gate: number): void {
-
-        this._gate = gate
-
-    }
-    public getVelocity(): number {
-
-        return this._velocity
-
-    }
-    public getGate(): number {
-
-        return this._gate
-
-    }
+    public setVelocity  (velocity: number)  : void      { this._velocity = velocity }
+    public setGate      (gate: number)      : void      { this._gate = gate }
+    public getVelocity  ()                  : number    { return this._velocity }
+    public getGate      ()                  : number    { return this._gate }
     
 }
