167ed6edae61e16d70c582c51df17f14f1135258
이혁기 <leestrument.github@gmail.com>
2021-02-03 08:18:45 +0900

    Cleaned LeeRange

--- api/helper/LeeRandom.ts
+++ api/helper/LeeRandom.ts
@@ -1,25 +1,5 @@
-const range = (min: number, max: number, isInteger = true): number => (
+const range = (min: number, max: number, isInteger = true): number => isInteger ? Math.floor(Math.random() * (max - min + 1) + min) : Math.random() * (max - min) + min
+const id    = () => Date.now().toString(16) + Math.random().toString(16) + '0'.repeat(16)
+const color = () => `hsl(${Math.floor(Math.random() * 360)}, 50%, 40%)`
 
-    isInteger ? Math.floor(Math.random() * (max - min + 1) + min) : Math.random() * (max - min) + min
-
-)
-
-const id = () => (
-
-    Date.now().toString(16) + Math.random().toString(16) + '0'.repeat(16)
-
-)
-
-const color = () => (
-
-    `hsl(${Math.floor(Math.random() * 360)}, 50%, 40%)`
-
-)
-
-export default {
-
-    range,
-    id,
-    color,
-
-}
+export default { range, id, color }
