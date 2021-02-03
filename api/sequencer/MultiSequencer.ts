b5c4a0691ec8fe7b053901f1aa9b94f02a658107
이혁기 <leestrument.github@gmail.com>
2021-02-03 09:27:19 +0900

    Cleaned MultiSequencer

--- api/sequencer/MultiSequencer.ts
+++ api/sequencer/MultiSequencer.ts
@@ -1,47 +1,21 @@
 import { PianoRoll }        from './PianoRoll'
 import { StepSequencer }    from './StepSequencer'
 
-export enum SequencerType {
-
-    PianoRoll,
-    StepSequencer,
-
-}
-
-const MULTI_SEQUENCER_TYPE_DEF = SequencerType.PianoRoll
+export enum SequencerType { PianoRoll, StepSequencer }
 
 export class MultiSequencer {
 
     constructor(
 
-        private _type               = MULTI_SEQUENCER_TYPE_DEF,
+        private _type               = SequencerType.PianoRoll,
         private _pianoRoll          = new PianoRoll,
         private _stepSequencer      = new StepSequencer
 
     ){}
 
-    // setter
-    public setType(type: SequencerType): void {
-
-        this._type = type
-
-    }
-
-    // getter 
-    public getType(): SequencerType {
-
-        return this._type
-
-    }
-    public getPianoRoll(): PianoRoll {
-
-        return this._pianoRoll
-
-    }
-    public getStepSequencer(): StepSequencer {
-
-        return this._stepSequencer
-
-    }
+    public setType          (type: SequencerType)   : void          { this._type = type }
+    public getType          ()                      : SequencerType { return this._type }
+    public getPianoRoll     ()                      : PianoRoll     { return this._pianoRoll }
+    public getStepSequencer ()                      : StepSequencer { return this._stepSequencer }
  
 }
