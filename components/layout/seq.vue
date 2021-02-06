3ecd5473e702f447c76fc2eda4fd35708e49d82c
이혁기 <leestrument.github@gmail.com>
2021-02-07 02:11:01 +0900

    Start from scratch

--- components/layout/seq.vue
+++ components/layout/seq.vue
@@ -1,51 +1,5 @@
 <template>
     
-    <div class='seq'>
-
-        <seq-header/>
-        <seq-timeline/>
-        <seq-editor/>
-        <seq-inspector/>
-
-    </div>
+    <div>Hello World!</div>
 
 </template>
-
-<script lang="ts">
-
-import { defineComponent }  from 'vue'
-import SeqHeader            from './seq-header.vue'
-import SeqTimeline          from '../timeline/seq-timeline.vue'
-import SeqEditor            from './seq-editor.vue'
-import SeqInspector         from './seq-inspector.vue'
-
-export default defineComponent({ components : { SeqHeader, SeqTimeline, SeqEditor, SeqInspector } })
-
-</script>
-
-<style scoped>
-
-    .seq {
-
-        width : 100%;
-        height : 100%;
-        background: rgb(35, 35, 40);
-
-        /* layout */
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-        grid-template-rows: 0.13fr 0.5fr 2.3fr;
-        gap: 0px 0px;
-        grid-template-areas:
-            "header header header header"
-            "timeline timeline timeline timeline"
-            "editor editor editor inspector";
-            
-    }
-
-    .seq-header     { grid-area: header; }
-    .seq-timeline   { grid-area: timeline; }
-    .seq-editor     { grid-area: editor; }
-    .seq-inspector  { grid-area: inspector; }
-
-</style> 
