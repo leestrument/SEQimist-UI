a7c426493aab8fdddd31e0040be7344eff74499c
이혁기 <leestrument.github@gmail.com>
2021-02-16 23:16:08 +0900

    Connected LeeSelectable to BaseTrack

--- api/track/BaseTrack.ts
+++ api/track/BaseTrack.ts
@@ -1,25 +1,23 @@
-export class BaseTrack {
+import { LeeSelectable } from '../helper/LeeSelectable'
+
+export class BaseTrack extends LeeSelectable {
 
     constructor(
 
         private _color      = `hsl(${Math.floor(Math.random() * 360)}, 50%, 40%)`,
         private _isActive   = true,
         private _isSolo     = false,
-        private _isSelected = false,
 
-    ){}
+    ){ super() }
 
     public setColor     (color: string) : void      { this._color       = color }
     public active       ()              : void      { this._isActive    = true }
     public mute         ()              : void      { this._isActive    = false }
     public solo         ()              : void      { this._isSolo      = true }
     public unsolo       ()              : void      { this._isSolo      = false }
-    public select       ()              : void      { this._isSelected  = true }
-    public deselect     ()              : void      { this._isSelected  = false }
     public getColor     ()              : string    { return this._color }
     public isActive     ()              : boolean   { return this._isActive }
     public isMuted      ()              : boolean   { return this._isActive === false }
     public isSolo       ()              : boolean   { return this._isSolo === true }
-    public isSelected   ()              : boolean   { return this._isSelected }
 
 }
